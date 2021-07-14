ea4c89d7eb831ac1478ecdd3c76305fc603270b4
Ole Ostergaard <ole.c.ostergaard@gmail.com>
2021-07-14 22:38:58 +0200

    Fix train_models.sh

--- impl/train_models.sh
+++ impl/train_models.sh
@@ -1,4 +1,4 @@
-train model() {
+train_model() {
 echo ---------- Training model for $1 ----------
 echo Running Coma for $1 on $2 data
 python main.py --coma-model-dir /abyss/home/tf-coma/coma-model --name $1 --data-dir /abyss/home/face-data/processed-data/$2 --mode train
@@ -6,15 +6,15 @@ python main.py --coma-model-dir /abyss/home/tf-coma/coma-model --name $1 --data-
 echo ----------------------------------------------
 }
 
-calculate_predictions lr8e3_bareteeth_coma bareteeth
-calculate_predictions lr8e3_cheeks_in_coma cheeks_in
-calculate_predictions lr8e3_eyebrow_coma eyebrow
-calculate_predictions lr8e3_high_smile_coma high_smile
-calculate_predictions lr8e3_lips_back_coma lips_back
-calculate_predictions lr8e3_lips_up_coma lips_up
-calculate_predictions lr8e3_mouth_down_coma mouth_down
-calculate_predictions lr8e3_mouth_extreme_coma mouth_extreme
-calculate_predictions lr8e3_mouth_middle_coma mouth_middle
-calculate_predictions lr8e3_mouth_open_coma mouth_open
-calculate_predictions lr8e3_mouth_side_coma mouth_side
-calculate_predictions lr8e3_mouth_up_coma mouth_up
+train_model lr8e3_bareteeth_coma bareteeth
+train_model lr8e3_cheeks_in_coma cheeks_in
+train_model lr8e3_eyebrow_coma eyebrow
+train_model lr8e3_high_smile_coma high_smile
+train_model lr8e3_lips_back_coma lips_back
+train_model lr8e3_lips_up_coma lips_up
+train_model lr8e3_mouth_down_coma mouth_down
+train_model lr8e3_mouth_extreme_coma mouth_extreme
+train_model lr8e3_mouth_middle_coma mouth_middle
+train_model lr8e3_mouth_open_coma mouth_open
+train_model lr8e3_mouth_side_coma mouth_side
+train_model lr8e3_mouth_up_coma mouth_up
