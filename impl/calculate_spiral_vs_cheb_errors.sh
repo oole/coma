757a7575f5e24a92fec2e375b89665422ab6970f
Ole Ostergaard <ole.c.ostergaard@gmail.com>
2022-01-10 12:20:07 +0100

    Add helper script to calculate and plot cheb vs spiral convolution.

---
+++ impl/calculate_spiral_vs_cheb_errors.sh
@@ -0,0 +1,28 @@
+calculate_error() {
+echo ---------- Calculating error for $1 ----------
+echo Calculating errors for CoMA and PCA
+python errors-spiral-vs-cheb.py --prediction-spiral results/$1_result.npy --prediction-spiral results/$2_result.npy --data-dir data/$3
+echo Finished
+echo ----------------------------------------------
+}
+
+
+# Execute calculate_predictions.sh first.
+# (split into two scripts, so that predictions and errors can be performed in different environments)
+# Calculates the error for the given model predictions nd the specified data
+# Extrapolation:
+calculate_error spiral_lr8e3_bareteeth_coma lr8e3_bareteeth_coma bareteeth
+calculate_error spiral_lr8e3_cheeks_in_coma lr8e3_cheeks_in_coma cheeks_in
+calculate_error spiral_lr8e3_eyebrow_coma lr8e3_eyebrow_coma  eyebrow
+calculate_error spiral_lr8e3_high_smile_coma lr8e3_high_smile_coma high_smile
+calculate_error spiral_lr8e3_lips_back_coma lr8e3_lips_back_coma lips_back
+calculate_error spiral_lr8e3_lips_up_coma lr8e3_lips_up_coma lips_up
+calculate_error spiral_lr8e3_mouth_down_coma lr8e3_mouth_down_coma mouth_down
+calculate_error spiral_lr8e3_mouth_extreme_coma lr8e3_mouth_extreme_coma mouth_extreme
+calculate_error spiral_lr8e3_mouth_middle_coma lr8e3_mouth_middle_coma mouth_middle
+calculate_error spiral_lr8e3_mouth_open_coma lr8e3_mouth_open_coma mouth_open
+calculate_error spiral_lr8e3_mouth_side_coma lr8e3_mouth_side_coma mouth_side
+calculate_error spiral_lr8e3_mouth_up_coma lr8e3_mouth_up_coma mouth_up
+
+# Interpolation:
+calculate_error spiral_lr8e3_sliced_coma lr8e3_sliced_coma sliced
---
+++ impl/errors-spiral-vs-cheb.py
@@ -0,0 +1,175 @@
+import argparse
+import numpy as np
+from data import meshdata
+from util.log_util import date_print
+import json
+import os
+from matplotlib import pyplot as plt
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--prediction-spiral", default="spiral-coma-model/results/spiral_lr8e3_sliced_coma_2201100844_result.npy",
+                    help="The path to the predictions.npy")
+parser.add_argument("--prediction-cheb", default="results/lr8e3_sliced_coma_result.npy",
+                    help="The path to the predictions.npy")
+parser.add_argument("--data-dir", default="data/sliced",
+                    help="The path to the data")
+parser.add_argument("--error-dir", default="spiral-coma-model/error-vs", help="The directory where the computed errors should be stored")
+parser.add_argument("--template-mesh", default="data/template.obj", help="The path to the template mesh")
+
+
+def cumulative_error(error):
+    num_bins = 1000
+    num_vertices, x_error = np.histogram(error, bins=num_bins)
+    x_error = np.convolve(x_error, 0.5 * np.ones(2), mode='valid')
+
+    factor_error = 100 / error.shape[0]
+
+    cumulative_num_vertices = np.cumsum(num_vertices)
+
+    x_error_vector = np.zeros((num_bins + 1,))
+    x_error_vector[1:] = x_error
+
+    y_vec_error = np.zeros((num_bins + 1,))
+    y_vec_error[1:] = factor_error * cumulative_num_vertices
+
+    return x_error_vector, y_vec_error
+
+
+def plot_error_over_vertices(cheb_coma_error, spiral_coma_error, pca_error, vertices, path):
+    cheb_coma_error = np.reshape(cheb_coma_error, (-1,))
+    spiral_coma_error = np.reshape(spiral_coma_error, (-1,))
+    pca_error = np.reshape(pca_error, (-1,))
+
+    x_cheb_coma, y_cheb_coma = cumulative_error(cheb_coma_error)
+    x_spiral_coma, y_spiral_coma = cumulative_error(spiral_coma_error)
+    x_pca, y_pca = cumulative_error(pca_error)
+
+    plt.plot(x_pca, y_pca, label="PCA")
+    plt.plot(x_cheb_coma, y_cheb_coma, label="CoMA C")
+    plt.plot(x_spiral_coma, y_spiral_coma, label="CoMA S")
+    plt.ylabel('Vertices (percent)')
+    plt.xlabel('Euclidean Error (mm)')
+
+    plt.legend(loc='lower right')
+    plt.xlim(0, 6)
+    plt.grid(True)  # ,color='grey', linestyle='-', linewidth=0.5)
+    plt.savefig(path)
+    pass
+
+
+def calculate_error(predicted_vertices, original_vertices):
+    error = np.sqrt(np.sum((predicted_vertices - original_vertices) ** 2, axis=2))
+    error_mean = np.mean(error)
+    error_std = np.std(error)
+    error_median = np.median(error)
+    return error, error_mean, error_std, error_median
+
+
+args = parser.parse_args()
+predictions_cheb = np.load(args.prediction_cheb)
+date_print("Calculating errors for " + args.prediction_cheb + " - Dir: " + args.data_dir)
+
+mesh_data = meshdata.MeshData(number_val=100, train_file=args.data_dir + '/train.npy',
+                              test_file=args.data_dir + '/test.npy',
+                              reference_mesh_file=args.template_mesh, fit_pca=True)
+
+# CoMA cheb
+date_print("Predicting using CoMA")
+predicted_cheb_vertices = (predictions_cheb * mesh_data.std) + mesh_data.mean
+
+# CoMA spiral
+predictions_spiral = np.load(args.prediction_spiral)
+predicted_spiral_vertices = (predictions_spiral * mesh_data.std) + mesh_data.mean
+
+# Original
+original_vertices = (mesh_data.vertices_test[:predictions_cheb.shape[0]] * mesh_data.std) + mesh_data.mean
+
+# we want millimeters
+predicted_cheb_vertices_mm = predicted_cheb_vertices * 1000
+predicted_spiral_vertices_mm = predicted_spiral_vertices * 1000
+original_vertices_mm = original_vertices * 1000
+
+# CoMA cheb
+cheb_model_error, cheb_model_error_mean, cheb_model_error_std, cheb_model_error_median = calculate_error(
+    predicted_cheb_vertices_mm,
+    original_vertices_mm)
+
+date_print(
+    "Cheb CoMA Error - Mean: " + str(cheb_model_error_mean) + ", Std: " + str(
+        cheb_model_error_std) + ", Median: " + str(
+        cheb_model_error_median))
+
+# CoMA spiral
+spiral_model_error, spiral_model_error_mean, spiral_model_error_std, spiral_model_error_median = calculate_error(
+    predicted_spiral_vertices_mm,
+    original_vertices_mm)
+
+date_print(
+    "Spiral CoMA Error - Mean: " + str(spiral_model_error_mean) + ", Std: " + str(
+        spiral_model_error_std) + ", Median: " + str(
+        spiral_model_error_median))
+
+# PCA
+date_print("Predicting using PCA")
+pca_prediction = mesh_data.pca.inverse_transform(mesh_data.pca.transform(
+    np.reshape(mesh_data.vertices_test, (mesh_data.vertices_test.shape[0], mesh_data.n_vertex * 3))))
+
+pca_vertices = (np.reshape(pca_prediction,
+                           (pca_prediction.shape[0], mesh_data.n_vertex, 3)) * mesh_data.std) + mesh_data.mean
+pca_vertices_mm = pca_vertices * 1000
+
+original_vertices = (mesh_data.vertices_test * mesh_data.std) + mesh_data.mean
+original_vertices_mm = original_vertices * 1000
+
+pca_error, pca_error_mean, pca_error_std, pca_error_median = calculate_error(pca_vertices_mm, original_vertices_mm)
+
+date_print("PCA Error - Mean: " + str(pca_error_mean) + ", Std: " + str(pca_error_std) + ", Median: " + str(
+    pca_error_median))
+
+date_print("Saving error plot")
+error_plot_path = args.error_dir + "/error_plot"
+if not os.path.exists(error_plot_path):
+    os.makedirs(error_plot_path)
+
+
+plot_error_over_vertices(cheb_model_error, spiral_model_error, pca_error, original_vertices,
+                         error_plot_path + "/" + os.path.basename(args.data_dir))
+
+date_print("Storing errors.")
+if not os.path.exists(args.error_dir):
+    os.makedirs(args.error_dir)
+# Cheb CoMA
+cheb_coma_error_file = args.error_dir + "/" + "cheb_coma_" + os.path.basename(args.data_dir)
+with open(cheb_coma_error_file + ".json", 'w') as file:
+    save_params = dict()
+    # save_params['coma_model_error'] = model_error
+    save_params['cheb_coma_model_error_mean'] = cheb_model_error_mean
+    save_params['cheb_coma_model_error_std'] = cheb_model_error_std
+    save_params['cheb_coma_model_error_median'] = cheb_model_error_median
+    date_print(str(save_params))
+    json.dump(save_params, file)
+np.save(cheb_coma_error_file + "_error.npy", cheb_model_error)
+
+# Spiral CoMA
+spiral_coma_error_file = args.error_dir + "/" + "spiral_coma_" + os.path.basename(args.data_dir)
+with open(spiral_coma_error_file + ".json", 'w') as file:
+    save_params = dict()
+    # save_params['coma_model_error'] = model_error
+    save_params['spiral_coma_model_error_mean'] = spiral_model_error_mean
+    save_params['spiral_coma_model_error_std'] = spiral_model_error_std
+    save_params['spiral_coma_model_error_median'] = spiral_model_error_median
+    date_print(str(save_params))
+    json.dump(save_params, file)
+np.save(spiral_coma_error_file + "_error.npy", spiral_model_error)
+
+# PCA
+pca_error_file = args.error_dir + "/" + "pca_" + os.path.basename(args.data_dir)
+with open(pca_error_file + ".json", 'w') as file:
+    save_params = dict()
+    # save_params['pca_model_error'] = pca_error
+    save_params['pca_model_error_mean'] = pca_error_mean
+    save_params['pca_model_error_std'] = pca_error_std
+    save_params['pca_model_error_median'] = pca_error_median
+    date_print(str(save_params))
+    json.dump(save_params, file)
+np.save(pca_error_file + "_error.npy", pca_error)
